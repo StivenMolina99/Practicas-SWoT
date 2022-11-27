a239b1502fe1357dd483645e0ed4ba6e14eb9367
StivenMolina99 <edwinsm@unicauca.edu.co>
2022-11-27 14:42:31 -0500

    Se completó la tarea

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,63 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put} from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface Producto{
+  nombre: string,
+  materia_prima: string,
+  duracion_dias: number
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  private persona = "Mundo";
+  private productos : Producto[] = [{
+    nombre: "Kumis",
+    materia_prima: "Leche",
+    duracion_dias: 45
+  }]
 
   @Get()
-  getHello(): string {
-    return `Hola: ${this.persona}`;
+  getHello(): Producto[] {
+    return this.productos;
   }
 
-  @Post(':nombre')
-  modificar(@Param('nombre') nombre: string): string {
-    this.persona = nombre;
-    return `Mensaje modificado: ${this.persona}`
+  @Post()
+  crear(@Body() datos: Producto): Producto {
+    this.productos.push(datos);
+    return datos;
+  }
+
+  @Put(":id")
+  modificar(@Body() datos: Producto, @Param('id') id: number): Producto | String{
+    try{
+      this.productos[id] = datos
+      return this.productos[id];
+    }
+    catch{
+      return `No fue posible modificar el Producto en la posición ${id}`
+    }
+  }
+
+  @Delete(":id")
+  eliminar(@Param('id') id: number){
+    try{
+      this.productos = this.productos.filter((val, index) => index != id);
+      return true;
+    }
+    catch{
+      return false;
+    }
+  }
+
+  @Patch(":id/duracion_dias/:duracion_dias")
+  cambiarDuracion(@Param('id') id: number, @Param('duracion_dias') duracion_dias: number): Producto | string{
+    try{
+      this.productos[id].duracion_dias = duracion_dias;
+      return this.productos[id];
+    }
+    catch{
+      return `No fue posible modificar al producto en la posición ${id}`
+    }
   }
 }
